5ddacfc6bfe1258d193edeb39795e0da9cb68b04
juposs <julianposs@gmx.de>
2019-07-14 17:30:21 +0200

    Check for existing values and finilize Ceph_Stats and Ceph_Recovery

--- python2.7/check_ceph.py
+++ python2.7/check_ceph.py
@@ -23,33 +23,90 @@ tb_total = float(bytes_total)/(2**40)
 print "P Ceph_Space TB_used={0:.2f}|Percent_used={1:.2f};{2};{3} Currently {1:f}%% used ({0:.2f} TiB of {4:.2f} TiB)".format(tb_used,perc_used,space_warn,space_crit,tb_total)
 
 #Ceph_Degraded
-degraded_objects = jsonfile["pgmap"]["degraded_objects"]
-degraded_total = jsonfile["pgmap"]["degraded_total"]
-degraded_ratio = jsonfile["pgmap"]["degraded_ratio"]
+try:
+    degraded_objects = jsonfile["pgmap"]["degraded_objects"]
+except KeyError:
+    degraded_objects = 0
+
+try:
+    degraded_total = jsonfile["pgmap"]["degraded_total"]
+except KeyError:
+    degraded_total = 0
+
+try:
+    degraded_ratio = jsonfile["pgmap"]["degraded_ratio"]
+except KeyError:
+    degraded_ratio = 0
+
+#print "{0} {1} {2}".format(degraded_objects, degraded_total, degraded_ratio)
 
 #Ceph_Misplaced
-misplaced_objects = jsonfile["pgmap"]["misplaced_objects"]
-misplaced_total = jsonfile["pgmap"]["misplaced_total"]
-misplaced_ratio = jsonfile["pgmap"]["misplaced_ratio"]
+try:
+    misplaced_objects = jsonfile["pgmap"]["misplaced_objects"]
+except KeyError:
+    misplaced_objects = 0
+
+try:
+    misplaced_total = jsonfile["pgmap"]["misplaced_total"]
+except KeyError:
+    misplaced_total = 0
+
+try:
+    misplaced_ratio = jsonfile["pgmap"]["misplaced_ratio"]
+except KeyError:
+    misplaced_ratio = 0
 
 #Ceph_Recovery
-recovering_objects_per_sec = jsonfile["pgmap"]["recovering_objects_per_sec"]
-recovering_bytes_per_sec = jsonfile["pgmap"]["recovering_bytes_per_sec"]
-recovering_keys_per_sec = jsonfile["pgmap"]["recovering_keys_per_sec"]
+try:
+    recovering_objects_per_sec = jsonfile["pgmap"]["recovering_objects_per_sec"]
+except KeyError:
+    recovering_objects_per_sec = 0
+
+try:
+    recovering_bytes_per_sec = jsonfile["pgmap"]["recovering_bytes_per_sec"]
+except KeyError:
+    recovering_bytes_per_sec = 0
+
+try:
+    recovering_keys_per_sec = jsonfile["pgmap"]["recovering_keys_per_sec"]
+except KeyError:
+    recovering_keys_per_sec = 0
+
+recovering_mb_sec = float(recovering_bytes_per_sec)/(2**20)
+
+print "0 Ceph_Recovery recovering_mb_sec={0:.2f}|recovering_objects_per_sec={1}|recovering_keys_per_sec={2} Recovery IO: {0:.2f} MiB/s | Objects/s: {1} | Keys/s: {2}".format(recovering_mb_sec, recovering_objects_per_sec, recovering_keys_per_sec)
 
 #Ceph_Stats
-read_bytes_sec = jsonfile["pgmap"]["read_bytes_sec"]
-write_bytes_sec  = jsonfile["pgmap"]["write_bytes_sec"]
-read_op_per_sec  = jsonfile["pgmap"]["read_op_per_sec"]
-write_op_per_sec  = jsonfile["pgmap"]["write_op_per_sec"]
+try:
+    read_bytes_sec = jsonfile["pgmap"]["read_bytes_sec"]
+except KeyError:
+    read_bytes_sec = 0
+
+try:
+    write_bytes_sec  = jsonfile["pgmap"]["write_bytes_sec"]
+except KeyError:
+    write_bytes_sec = 0
+
+try:
+    read_op_per_sec  = jsonfile["pgmap"]["read_op_per_sec"]
+except KeyError:
+    read_op_per_sec = 0
+
+try:
+    write_op_per_sec  = jsonfile["pgmap"]["write_op_per_sec"]
+except KeyError:
+    write_op_per_sec = 0
+
+read_mb_sec = float(read_bytes_sec)/(2**20)
+write_mb_sec = float(write_bytes_sec)/(2**20)
+
+print "0 Ceph_Stats read_mb_sec={0:.2f}|write_mb_sec={1:.2f}|read_op_per_sec={2}|write_op_per_sec={3} Client IO/read: {0:.2f} MiB/s | Client IO/write: {1:.2f} MiB/s | OP/s Read: {2} | OP/s Write: {3}".format(read_mb_sec, write_mb_sec, read_op_per_sec, write_op_per_sec)
 
 #Ceph_Scrubbing
 
 #Ceph_Health
 
-# TODO Check if the values exist for all except heatlh
+# Create new checks Ceph_Misplaced and Ceph_Degraded, or just include them into Ceph_Health?
 # Old outputs
 #echo "$healthstatus Ceph_Health - $STATE"
-#echo "0 Ceph_Stats io_read_mibs=$io_read|io_write_mibs=$io_write|ops_rd_sec=$ops_read|ops_wr_sec=$ops_write IO/read: $io_read MiB/s - IO/write: $io_write MiB/s - OP/s Read: $ops_read - OP/s Write: $ops_write"
-#echo "0 Ceph_Recovery rec_io_mibs=$recovery_io|obj_sec=$recovery_obj $recovery_info"
 #echo "0 Ceph_Scrubbing deep_scrub=$deep_scrub|scrubbing=$scrub $scrubbing_info"
