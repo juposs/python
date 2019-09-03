252e36880a12936febd0f091a0eb546227f94e87
juposs <julianposs@gmx.de>
2019-09-03 16:10:03 +0200

    myutil now installs in own folder, see TODOs (6)

---
+++ python3/build/lib/myutil/myutil_defaults.py
@@ -0,0 +1,12 @@
+#myldap defaults
+default_match_attribute="userPrincipalName"
+default_dn="OU=OrgUnit,DC=example,DC=org"
+default_ldap_server="example.org"
+
+#mail defaults
+default_mail_server="mailserver.example.org"
+default_mail_port="25"
+default_sendfile="false"
+default_filepath="None"
+default_sender="no-reply@example.org"
+default_mail_password="None"
--- python3/setup_myuitl.py
+++ python3/setup_myuitl.py
@@ -4,9 +4,12 @@ import setuptools
 
 requires = ['python3-ldap']
 
+setup_kwargs = {'packages': ["myutil"],
+                'package_dir': {'': "."}}
+
 setuptools.setup(
      name='myutil',
-     version='2.3',
+     version='2.4',
      py_modules=['myutil', 'myutil_defaults'],
      #package_dir={"":"."},
      download_url="",
@@ -18,11 +21,12 @@ setuptools.setup(
      long_description="A python utility package for querying ldap, sending mails and working with files",
      #long_description_content_type="text/markdown",
      url="https://github.com/juposs/python/tree/master/python3/myutil",
-     packages=setuptools.find_packages(),
+     #packages=setuptools.find_packages(),
      classifiers=[
          "Programming Language :: Python :: 3.6",
          "License :: OSI Approved :: undefined",
          "Operating System :: OS Independent",
      ],
-     install_requires=requires
+     install_requires=requires,
+      **setup_kwargs
  )
