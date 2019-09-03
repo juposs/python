dd36aa1c4a72ee498ff158c2907c87fd16bc264d
juposs <julianposs@gmx.de>
2019-09-03 13:24:40 +0200

    build first module for python3

--- python3/myutil/setup.py
+++ python3/myutil/setup.py
@@ -2,21 +2,21 @@ import setuptools
 
 #optional = ['ansicolors', 'mock>=1.0.1']
 
-requires = ['python-ldap']
+requires = ['python3-ldap']
 
 setuptools.setup(
      name='myutil.py',
-     version='1.1',
+     version='2.0',
      py_modules=['myutil'],
      author="Julian Poss",
      author_email="john.doe@gmail.com",
      description="A python utility package",
      long_description="A python utility package for querying ldap, sending mails and working with files",
      long_description_content_type="text/markdown",
-     url="https://github.com/juposs/python/tree/master/python2.7/myutil",
+     url="https://github.com/juposs/python/tree/master/python3/myutil",
      packages=setuptools.find_packages(),
      classifiers=[
-         "Programming Language :: Python :: 2.7",
+         "Programming Language :: Python :: 3.6",
          "License :: OSI Approved :: undefined",
          "Operating System :: OS Independent",
      ],
